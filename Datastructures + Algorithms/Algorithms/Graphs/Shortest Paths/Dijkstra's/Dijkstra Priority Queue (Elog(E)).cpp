fdefc99c232dda61e76397a93379010526bdd23f
Elijah-Huang <elijahjhuang@gmail.com>
2021-03-28 16:34:12 -0700

    Dijkstra Template + Testing

---
+++ Datastructures + Algorithms/Algorithms/Graphs/Shortest Paths/Dijkstra's/Dijkstra Priority Queue (Elog(E)).cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<vector<pair<int, int>>> adj;
+
+vector<long long> dijkstra_pq(int start) {
+	vector<long long> dist(n + 1, 1ll << 62);
+	dist[start] = 0;
+	priority_queue<tuple<long long, int>, vector<tuple<long long, int>>, greater<tuple<long long, int>>> pq;
+	vector<bool> visited(n + 1);
+	long long d, new_d; int node;
+
+	pq.push({ 0,start });
+	while (!pq.empty()) {
+		tie(d, node) = pq.top(); pq.pop();
+
+		if (!visited[node]) {
+			visited[node] = 1;
+
+			for (auto& [neighbor, w] : adj[node]) {
+				new_d = d + w;
+				if (new_d < dist[neighbor]) {
+					dist[neighbor] = new_d;
+					pq.push({ new_d, neighbor });
+				}
+			}
+		}
+	}
+
+	return dist;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> n >> m;
+	adj.resize(n + 1);
+
+	for (int i = 0; i < m; i++) {
+		int a, b, w; cin >> a >> b >> w;
+		adj[a].push_back({ b,w });
+		adj[b].push_back({ a,w });
+	}
+}
---
+++ Problems/Assorted OJ/CSES/Graph Algorithms/Shortetst Routes 1(n^2 dijksta test).cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<vector<pair<int, int>>> adj;
+
+vector<long long> dijkstra(int start) {
+	vector<long long> dist(n + 1, 1ll<<62);
+	dist[start] = 0;
+	vector<bool> visited(n + 1);
+
+	for (int iters = 0; iters < n; iters++) {
+		long long min_dist = 1ll<<62; int min_node = -1;
+
+		for (int j = 1; j <= n; j++) {
+			if (!visited[j] and min_dist > dist[j]) {
+				min_dist = dist[j];
+				min_node = j;
+			}
+		}
+
+		visited[min_node] = 1;
+		for (auto& [neighbor, w] : adj[min_node]) {
+			dist[neighbor] = min(dist[neighbor], min_dist + w);
+		}
+	}
+
+	return dist;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> n >> m;
+	adj.resize(n + 1);
+
+	for (int i = 0; i < m; i++) {
+		int a, b, w; cin >> a >> b >> w;
+		adj[a].push_back({ b,w });
+		//adj[b].push_back({ a,w });
+	}
+
+	vector<long long> dist = dijkstra(1);
+	for (int i = 1; i <= n; i++) {
+		cout << dist[i] << ' ';
+	}
+}
