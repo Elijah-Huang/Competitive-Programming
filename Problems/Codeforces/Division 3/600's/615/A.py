d8b31435901016e9d38413511741b0a54e19439e
Elijah-Huang <elijahjhuang@gmail.com>
2021-03-29 20:06:14 -0700

    lockout

--- Problems/Codeforces/Division 3/600's/615/A.py
+++ Problems/Codeforces/Division 3/600's/615/A.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-    a,b,c,n = map(int,input().split())
+    a, b, c, n = map(int, input().split())
     tot = a + b + c + n
 
     if tot % 3 == 0 and tot//3 >= max(a,b,c):
