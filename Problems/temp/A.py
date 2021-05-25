90c94b4a2ae613168f7d70f7a2ae3d30114a8522
Elijah-Huang <elijahjhuang@gmail.com>
2021-05-25 16:34:43 -0700

    lockout

--- Problems/temp/A.py
+++ Problems/temp/A.py
@@ -1,4 +1,10 @@
 import sys
 input = sys.stdin.buffer.readline
 
-for _ in range(int(input())):
+mod = 998244353
+
+n,k = map(int,input().split())
+
+dp = [[[0]*2 for j in range(n+1)] for i in range(n+1)] # [pos][mx][curr color]
+
+dp[0][1][1] = dp[0][1][1] = 0
---
+++ Problems/temp/C.py
@@ -0,0 +1,4 @@
+import sys
+input = sys.stdin.buffer.readline
+
+for _ in range(int(input())):
