162bd26b2270cbdfd4117e1578ea7e057d168ca6
Elijah-Huang <elijahjhuang@gmail.com>
2021-05-30 11:59:47 -0700

    Update Choose O(1) with O(n) Precomputation.py

--- Datastructures + Algorithms/Algorithms/CP algorithms/Combinatorics/Mod Choose/Choose in O(1) with O(n) precomputation/Choose O(1) with O(n) Precomputation.py
+++ Datastructures + Algorithms/Algorithms/CP algorithms/Combinatorics/Mod Choose/Choose in O(1) with O(n) precomputation/Choose O(1) with O(n) Precomputation.py
@@ -1,14 +1,17 @@
+maxn = 10**5
 mod = 10**9 + 7
-fac = [0]*(n+1)
-inv_fac = [0]*(n+1)
+fac = [0]*(maxn+1)
+inv_fac = [0]*(maxn+1)
 
 fac[0] = 1
-for i in range(1,n+1):
+for i in range(1,maxn+1):
     fac[i] = fac[i-1] * i % mod
 
-inv_fac[n] = pow(fac[n],mod-2,mod)
-for i in range(n-1,-1,-1):
+inv_fac[maxn] = pow(fac[maxn],mod-2,mod)
+for i in range(maxn-1,-1,-1):
     inv_fac[i] = inv_fac[i+1] * (i+1) % mod
 
 def choose(n,k):
+    if k > n:
+        return 0
     return fac[n] * inv_fac[k] * inv_fac[n-k] % mod
